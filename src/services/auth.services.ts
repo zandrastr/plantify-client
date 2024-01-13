33a207c79a0d7e168c4ea105cbc27a8729b5578a
Sandra S <zandrastr@hotmail.com>
2024-01-13 20:58:09 +0000

    create service for login

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { IUser } from "../models/user.model";
 
 const SERVER_API_URL = import.meta.env.VITE_SERVER_API_URL;
 
@@ -12,4 +13,17 @@ export const signup = async (requestBody: object):Promise<{email: string}> => {
     };
     const { data } = await axios.post(`${SERVER_API_URL}/signup`, requestBody, options);
     return data;
+}
+
+// ***************** Post user login data ****************
+export const login = async (requestBody: object):Promise<IUser> => {
+
+  const options = {
+      headers: {
+          'Content-Type': 'application/json',
+      }
+  };
+
+  const { data } = await axios.post(`${SERVER_API_URL}/login`, requestBody, options);
+  return data;
 }
