7a2740a8d1c0d46d1f69013981ed165297783837
Sandra S <zandrastr@hotmail.com>
2024-01-23 11:37:31 +0000

    add remove plant functionality to favorites page

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,38 +1,80 @@
-import { Box, Button, HStack, Heading, Image, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, HStack, Heading, Image, Text, VStack, useToast, useDisclosure } from '@chakra-ui/react';
 import { IPlantModel } from '../models/plant.model';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { IUserContext, UserContext } from '../contexts/userContext';
+import { removePlant } from '../services/user.services';
+import RemovePlantModal from '../components/RemovePlantModal';
 
 const FavoritesPage = () => {
-  const { currentUser } = useContext(UserContext) as IUserContext;
+  const { currentUser, removeFromFavorites } = useContext(UserContext) as IUserContext;
   const userFavorites = currentUser!.favorites;
-
-  const handleRemove = () => {
-    console.log('Clicked to remove a plant from favorites');
-  };
+  const [selectedPlantToRemove, setSelectedPlantToRemove] = useState<IPlantModel | null>(null);
+  const { isOpen: isRemoveModalOpen, onOpen: onOpenRemoveModal, onClose: onCloseRemoveModal } = useDisclosure();
+  const toast = useToast();
 
   const openPlantModal = () => {
     console.log('Clicked on a favorite to open plant modal');
   };
 
+  const handleRemove = async (plant: IPlantModel) => {
+    onOpenRemoveModal();
+    setSelectedPlantToRemove(plant);
+  };
+
+  const confirmRemove = async (plantId: string) => {
+    if (currentUser && plantId) {
+      try {
+        await removePlant(currentUser._id, plantId);
+        removeFromFavorites(plantId);
+        handleToast();
+      } catch (error) {
+        console.error('Something went wrong:', error);
+      }
+    }
+  };
+
+  const handleToast = () => {
+    toast({
+      description: 'Plant removed from favorites',
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+      position: 'top-right',
+    });
+    onCloseRemoveModal();
+  };
+
   return (
     <>
       <Heading>Favorites</Heading>
-
-      {userFavorites.map((plant: IPlantModel) => (
-        <Box key={plant._id}>
-          <HStack>
-            <HStack onClick={openPlantModal}>
-              <Image boxSize='50px' src={plant.imageUrl} fallbackSrc='https://via.placeholder.com/50' alt={plant.name} />
-              <VStack>
-                <Text>{plant.name}</Text>
-                <Text>{plant.latinName}</Text>
-              </VStack>
-            </HStack>
-            <Button onClick={handleRemove}>-</Button>
-          </HStack>
-        </Box>
-      ))}
+      {userFavorites.length > 0 ? (
+        <>
+          {userFavorites.map((plant: IPlantModel) => (
+            <Box key={plant._id}>
+              <HStack>
+                <HStack onClick={openPlantModal}>
+                  <Image boxSize='50px' src={plant.imageUrl} fallbackSrc='https://via.placeholder.com/50' alt={plant.name} />
+                  <VStack>
+                    <Text>{plant.name}</Text>
+                    <Text>{plant.latinName}</Text>
+                  </VStack>
+                </HStack>
+                <Button onClick={() => handleRemove(plant)}>-</Button>
+              </HStack>
+            </Box>
+          ))}
+        </>
+      ) : (
+        <Text>No plants saved yet</Text>
+      )}
+      <RemovePlantModal
+        message={`Do you want to remove ${selectedPlantToRemove!.name} from favorites?`}
+        buttonText='Confirm Remove'
+        isOpen={isRemoveModalOpen}
+        onClose={onCloseRemoveModal}
+        onOpen={onOpenRemoveModal}
+        mainFunction={() => confirmRemove(selectedPlantToRemove!._id!)}
+      />
     </>
   );
 };
