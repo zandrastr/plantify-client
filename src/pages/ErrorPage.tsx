66d2ea13fdf132f026bb502d1c6b57e361542c44
Sandra S <zandrastr@hotmail.com>
2024-01-24 15:52:14 +0000

    create error page structure with link to startpage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,16 @@
-import { Box } from '@chakra-ui/react';
+import { Heading, Image, Link as ChakraLink, VStack } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <>
-      <Box>Error page</Box>
-    </>
+    <VStack>
+      <Image boxSize='150px' src='' fallbackSrc='https://via.placeholder.com/150' alt='' />
+      <Heading>Oops, something went wrong!</Heading>
+
+      <ChakraLink textDecoration='underline' as={ReactRouterLink} to='/'>
+        Go back to startpage
+      </ChakraLink>
+    </VStack>
   );
 };
 
