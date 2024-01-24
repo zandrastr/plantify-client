5fdd969d71f82fd65d2e251925bc79a5fce3e83f
Sandra S <zandrastr@hotmail.com>
2024-01-24 12:08:43 +0000

    create service to update user profile

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { IUser } from "../models/user.model";
 
 const SERVER_API_URL = import.meta.env.VITE_SERVER_API_URL;
 
@@ -33,5 +34,24 @@ export const removePlant = async (userId: string, plantId:string):Promise<{messa
     };
 
     const { data } = await axios.put(`${SERVER_API_URL}/users/favorites/remove`, requestBody, options);
+    return data;
+};
+
+//***************** Update user profile ****************
+export const updateUser = async (userId : string, email : string, password : string, name : string):Promise<{message: string, userInfo: IUser}> => {
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    const requestBody = {
+        userId,
+        email,
+        password, 
+        name
+    };
+
+    const { data } = await axios.put(`${SERVER_API_URL}/users/edit`, requestBody, options);
     return data;
 };
